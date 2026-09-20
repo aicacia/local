55863e286aa91a16d58471669e6a1d2b882fca37
Nathan Faucett <nathanfaucett@gmail.com>
2026-09-20 13:51:58 -0400

    Refactor file system to use deckv for metadata and introduce content storage under `file_id`

--- crates/iroh-chain-file-system/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![forbid(unsafe_code)]
-
-mod scoped_transport;
-
-pub use scoped_transport::{AccessTokenProvider, ScopedIrohTransport, StaticAccessToken};
--- crates/iroh-chain/src/store.rs
+++
@@ -1,5 +0,0 @@
-use iroh::EndpointId;
-
-pub trait AllowedEndpointId: Send + Sync + 'static {
-    fn allowed(&self, id: EndpointId) -> impl Future<Output = bool> + Send;
-}
